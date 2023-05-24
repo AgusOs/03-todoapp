0b859c833e373a8a193ae6a3322900ec1d6a413d
Agustin Osorio <osorioagus96@gmail.com>
2023-05-24 13:38:24 -0300

    51

---
+++ src/app/filter/filter.reducer.ts
@@ -0,0 +1,8 @@
+import { createReducer, on } from "@ngrx/store";
+import { setFilter, validFilters } from './filter.actions';
+
+export const initialState: validFilters = 'all' as validFilters;
+
+export const filterReducer = createReducer(initialState,
+    on( setFilter, (state, { filter }) => filter )
+);
